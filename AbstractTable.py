6d8461cb2dd07fb7a3926b108b409975e61a0464
George Riddle <george.riddle@outlook.com>
2021-09-12 13:28:57 -0400

    Longer prefix for display start working

--- AbstractTable.py
+++ AbstractTable.py
@@ -1,6 +1,6 @@
 from ipaddress import ip_network
 from PyQt6 import QtCore, QtWidgets
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import QSize, QWaitCondition, Qt
 import databuilder
 
 class TableModel(QtCore.QAbstractTableModel):
@@ -23,43 +23,57 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        def fix():
-            data[2][1] = "192.168.1.128/25"
-            data[2][2] = "192.168.1.128/24"
-            model.layoutChanged.emit()
-
         self.setWindowTitle("IP-Visualizer")
         self.setGeometry(100, 100, 400, 400)
 
         widget = QtWidgets.QWidget()
+        self.table = QtWidgets.QTableView()
+        self.labelNetwork = QtWidgets.QLabel("Network to Visualze as CIDR")
+        self.displayNetwork = QtWidgets.QLineEdit("192.168.1.0/24")
+        self.displayNetwork.setMaxLength(18)
+        size0 = QSize(10,10)
+        self.displayNetwork.setMaximumWidth(125)
+        self.labelStart = QtWidgets.QLabel("Start")
+        self.displayStart = QtWidgets.QLineEdit("24")
+        self.displayStart.setMaxLength(2)
+        self.displayStart.setMaximumWidth(25)
+        self.labelEnd = QtWidgets.QLabel("end")
+        self.displayEnd = QtWidgets.QLineEdit("26")
+        self.displayEnd.setMaximumWidth(25)
+        self.displayEnd.setMaxLength(2)
 
-        visualNet = ip_network("143.0.0.0/14")
-        start = 14
-        end = 32
-        data = databuilder.buildDisplayList(visualNet, start, end)
-        table = QtWidgets.QTableView()
-
-        columnCount =  end - start + 1
-        rowCount = 2 ** (end - start)
-        for currentrow in range(0,rowCount):
-            for currentcol in range(0,columnCount):
-                if data[currentrow][currentcol].get('network'):
-                    span = data[currentrow][currentcol].get('spansize')
-                    if span > 1:
-                        table.setSpan(currentrow, currentcol, span, 1)
-        model = TableModel(data)
-        table.setModel(model)
+        self.btnGenerate = QtWidgets.QPushButton('Generate')
+        self.btnGenerate.clicked.connect(self.generate)
+        
         self.setCentralWidget(widget)
         self.setGeometry(600, 100, 400, 200)
-
-        update = QtWidgets.QPushButton("update")
-        update.clicked.connect(fix)
-
+    
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(table)
-        layout.addWidget(update)
+        layout.addWidget(self.labelNetwork)
+        layout.addWidget(self.displayNetwork)
+        layout.addWidget(self.labelStart)
+        layout.addWidget(self.displayStart)
+        layout.addWidget(self.labelEnd)
+        layout.addWidget(self.displayEnd)
+        layout.addWidget(self.btnGenerate)
+        layout.addWidget(self.table)
         widget.setLayout(layout)
-
+    
+    def generate(self):
+        net = ip_network(self.displayNetwork.text())
+        start = int(self.displayStart.text())
+        end = int(self.displayEnd.text())
+        columnCount =  end - start + 1
+        rowCount = 2 ** (end - net.prefixlen)
+        self.data = databuilder.buildDisplayList(net, start, end)
+        model = TableModel(self.data)
+        self.table.setModel(model)
+        for currentrow in range(0,rowCount):
+            for currentcol in range(0,columnCount):
+                if self.data[currentrow][currentcol].get('network'):
+                    span = self.data[currentrow][currentcol].get('spansize')
+                    if span > 1:
+                        self.table.setSpan(currentrow, currentcol, span, 1)
 
 app = QtWidgets.QApplication([])
 window = MainWindow()
--- databuilder.py
+++ databuilder.py
@@ -14,7 +14,7 @@ def makeEmptygrid(cCount: int, rCount:int) -> list[list]:
 def buildDisplayList(cidr: ip_network, startPrefix: int, endPrefix: int):
     ''' Returns a list of list, each being a subnet column '''
     columnCount =  endPrefix - startPrefix + 1
-    rowCount = 2 ** (endPrefix - startPrefix)
+    rowCount = 2 ** (endPrefix - cidr.prefixlen)
     data = makeEmptygrid(columnCount,rowCount)
     colNum = 0
     for prefix in list(range(startPrefix, endPrefix + 1)):
@@ -25,7 +25,7 @@ def buildDisplayList(cidr: ip_network, startPrefix: int, endPrefix: int):
             for net in sbnet:
                 data[rowNum][colNum] = {'network': net.with_prefixlen,
                                         'spansize': 2 ** (endPrefix - prefix)}
-                rowNum += 2 ** (endPrefix - prefix)
+                rowNum += (2 ** (endPrefix - prefix))
         elif prefix < cidr.prefixlen:
             rowNum = 0
             sbnet = cidr.supernet(new_prefix=prefix)
@@ -33,7 +33,7 @@ def buildDisplayList(cidr: ip_network, startPrefix: int, endPrefix: int):
                 network = ip_network(net)
                 data[rowNum][colNum] = {'network': network.with_prefixlen,
                                         'spansize': 2 ** (endPrefix - prefix)}
-                rowNum += 2 ** (endPrefix - prefix)
+                rowNum += 2 ** (endPrefix - prefix) 
         
         colNum += 1
     return data
@@ -41,20 +41,18 @@ def buildDisplayList(cidr: ip_network, startPrefix: int, endPrefix: int):
 
 if __name__ == '__main__':
     network = ip_network("192.168.1.0/24")
-    start = 24
+    start = 25
     end = 27
     networks = buildDisplayList(network, start, end)
-    for item in networks:
-        print(item)
+    #for item in networks:
+    #    print(item)
     columnCount =  end - start + 1
-    rowCount = 2 ** (end - start)
+    rowCount = 2 ** (end - network.prefixlen)
     for currentrow in range(0,rowCount):
         for currentcol in range(0,columnCount):
             if networks[currentrow][currentcol]:
                 print(networks[currentrow][currentcol])
             else:
                 print('!')
-
-    print('!' * 80)
 
 
