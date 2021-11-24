fc1e5fb1da7eb411a0c98899b8740f723d01f89f
George Riddle <george.riddle@outlook.com>
2021-11-23 21:05:46 -0500

    Toolbar added save/load buttons removed

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt6 import QtCore, QtWidgets
 from PyQt6 import QtGui
 from PyQt6.QtCore import Qt, QRegularExpression
 from PyQt6.QtGui import (
+    QAction,
+    QIcon,
     QIntValidator,
     QRegularExpressionValidator,
 )
@@ -41,25 +43,42 @@ class TableModel(QtCore.QAbstractTableModel):
 
 
 class MainWindow(QtWidgets.QMainWindow):
+    matchcidr = QRegularExpression(
+            r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$"
+        )
+    cidrRegex = QRegularExpressionValidator(QRegularExpression(matchcidr))
     def __init__(self):
         super().__init__()
         self.user_fields = {}
         self.zero32 = QIntValidator(0, 32)
-        matchcidr = QRegularExpression(
-            r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$"
-        )
-        cidrRegex = QRegularExpressionValidator(QRegularExpression(matchcidr))
         self.setWindowTitle("IP-Visualizer")
         self.setGeometry(50, 50, 1000, 1200)
 
+        toolbar = QtWidgets.QToolBar("Tool Bar Title")
+        self.addToolBar(toolbar)
+
+        loadIcon = QtGui.QIcon("icons/database--plus.png")
+        loadAction = QtGui.QAction(loadIcon, "Load", self)
+        loadAction.setStatusTip("Load Data!")
+        loadAction.triggered.connect(self.getFile)
+
+        saveicon = QtGui.QIcon("icons/disk-return.png")
+        saveAction = QtGui.QAction(saveicon, "Save It!", self)
+        saveAction.setStatusTip("save data to file")
+        saveAction.triggered.connect(self.save)
+
+        abouticon = QtGui.QIcon("icons/question-button.png")
+        aboutAction = QtGui.QAction(abouticon, "About", self)
+
+        toolbar.addAction(loadAction)
+        toolbar.addAction(saveAction)
+        toolbar.addAction(aboutAction)
+
+        printAction = QtGui.QAction("Print It!", self)
+        printAction.triggered.connect(print)
+
         menubar = QtWidgets.QMenuBar()
         menubar.setMaximumWidth(50)
-        fileMenu = menubar.addMenu("File")
-        fileMenu.addAction("New Dataset")
-        fileMenu.addAction("Open a Dataset")
-        fileMenu.addAction("Save a Dataset")
-        fileMenu.addAction("Print")
-        fileMenu.addAction("Exit")
 
         self.setStatusBar(QtWidgets.QStatusBar(self))
         self.saveData = {}
@@ -73,7 +92,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.displayNetwork = QtWidgets.QLineEdit("192.168.1.0/24")
         self.displayNetwork.setMaxLength(18)
         self.displayNetwork.setMaximumWidth(125)
-        self.displayNetwork.setValidator(cidrRegex)
+        self.displayNetwork.setValidator(self.cidrRegex)
 
         self.labelStart = QtWidgets.QLabel("Start")
         self.displayStart = QtWidgets.QLineEdit("24")
@@ -98,13 +117,7 @@ class MainWindow(QtWidgets.QMainWindow):
         fields_section.setMaximumWidth(300)
         fieldlayout = QtWidgets.QFormLayout()
         fields_section.setLayout(fieldlayout)
-        filebtn = QtWidgets.QPushButton("Load")
-        filebtn.clicked.connect(self.getFile)
-        fieldlayout.addRow(filebtn)
 
-        savebtn = QtWidgets.QPushButton("Save")
-        savebtn.clicked.connect(self.save)
-        fieldlayout.addRow(savebtn)
         self.ufields = {'key': QtWidgets.QLineEdit()}
         self.getFile()
         self.update_user_fields()
@@ -129,18 +142,14 @@ class MainWindow(QtWidgets.QMainWindow):
         widget.setLayout(layout)
 
     def update_user_fields(self):
-        print(f"User_fields is {self.user_fields}")
         for val in self.user_fields.keys():
             if self.user_fields[val]["controlType"] == "lineEdit":
                 self.ufields[val] = QtWidgets.QLineEdit()
             if self.user_fields[val]['controlType'] == 'checkbox':
                 self.ufields[val] = QtWidgets.QCheckBox()
 
-    def selectFile(self):
-        return QtWidgets.QFileDialog.getOpenFileName(self, "Choose file")
-
     def getFile(self):
-        file = self.selectFile()
+        file = QtWidgets.QFileDialog.getOpenFileName(self, "Choose file")
         if file[0]:
             with open(file[0], "r") as F1:
                 self.saveData = json.load(F1)
@@ -148,6 +157,8 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             print("Failed to load File")
             self.statusTip = "Failed to Load File!"
+    
+
 
     def updateRecord(self):
         self.setStatusTip("Time to update SaveData!")
@@ -166,13 +177,18 @@ class MainWindow(QtWidgets.QMainWindow):
                         self.saveData[key][ref] = True
 
     def save(self):
-        fileToSave = self.selectFile()
+        fileToSave = QtWidgets.QFileDialog.getSaveFileName(self, "WHERE?")
         if fileToSave[0]:
             with open(fileToSave[0], "w") as F1:
                 json.dump(self.saveData, F1, indent=4)
         else:
             print("Save Failed, did not get a file from file dialog")
             # Todo: Make this message appear in status Bar.
+
+    def print(self):
+        print("doing the printing thing!")
+        dialog = QtGui.QPrintDialog()
+
 
     def clearUfileds(self):
         for key, control in self.ufields.items():
