eeec9e9b20c7dbe2b74898012fb3cad9a4af1830
George Riddle <george.riddle@outlook.com>
2021-11-27 17:41:26 -0500

    Priority coloring

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class TableModel(QtCore.QAbstractTableModel):
         if role == Qt.ItemDataRole.DisplayRole:
             out = ''
             for key, value in item.items():
-                if key not in ['color', 'spansize']:
+                if key not in ['spansize']:
                     if key == 'network':
                         out += f'{value}\n'
                     else:
@@ -49,7 +49,7 @@ class MainWindow(QtWidgets.QMainWindow):
     cidrRegex = QRegularExpressionValidator(QRegularExpression(matchcidr))
     def __init__(self):
         super().__init__()
-        self.user_fields = {}
+        self.fieldsData = {}
         self.zero32 = QIntValidator(0, 32)
         self.setWindowTitle("IP-Visualizer")
         self.setGeometry(50, 50, 1000, 1200)
@@ -60,18 +60,23 @@ class MainWindow(QtWidgets.QMainWindow):
         loadIcon = QtGui.QIcon("icons/database--plus.png")
         loadAction = QtGui.QAction(loadIcon, "Load", self)
         loadAction.setStatusTip("Load Data!")
-        loadAction.triggered.connect(self.getFile)
+        loadAction.triggered.connect(self.loadSaveData)
 
         saveicon = QtGui.QIcon("icons/disk-return.png")
-        saveAction = QtGui.QAction(saveicon, "Save It!", self)
+        saveAction = QtGui.QAction(saveicon, "Save", self)
         saveAction.setStatusTip("save data to file")
         saveAction.triggered.connect(self.save)
 
-        abouticon = QtGui.QIcon("icons/question-button.png")
-        aboutAction = QtGui.QAction(abouticon, "About", self)
+        printIcon = QtGui.QIcon("icons/printer.png")
+        printAction = QtGui.QAction(printIcon, "Print", self)
+        printAction.setStatusTip("Not Implemented")
+        aboutIcon = QtGui.QIcon("icons/question-button.png")
+        aboutAction = QtGui.QAction(aboutIcon, "About", self)
+        aboutAction.setStatusTip("Not Implemented")
 
         toolbar.addAction(loadAction)
         toolbar.addAction(saveAction)
+        toolbar.addAction(printAction)
         toolbar.addAction(aboutAction)
 
         printAction = QtGui.QAction("Print It!", self)
@@ -83,7 +88,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setStatusBar(QtWidgets.QStatusBar(self))
         self.saveData = {}
         self.filebtn = QtWidgets.QPushButton("Open File")
-        self.filebtn.clicked.connect(self.getFile)
+        self.filebtn.clicked.connect(self.loadSaveData)
         widget = QtWidgets.QWidget()
         self.table = QtWidgets.QTableView()
         self.table.clicked.connect(self.showSelection)
@@ -110,22 +115,28 @@ class MainWindow(QtWidgets.QMainWindow):
         self.btnGenerate.setMaximumWidth(100)
         self.btnGenerate.clicked.connect(self.generate)
 
+        self.openfile = QtWidgets.QLabel("NONE")
         self.setCentralWidget(widget)
         self.setGeometry(600, 100, 400, 200)
 
-        fields_section = QtWidgets.QWidget()
+        fields_section = QtWidgets.QWidget(None)
         fields_section.setMaximumWidth(300)
         fieldlayout = QtWidgets.QFormLayout()
         fields_section.setLayout(fieldlayout)
 
         self.ufields = {'key': QtWidgets.QLineEdit()}
-        self.getFile()
+        self.loadSaveData()
+        deleteIcon = QtGui.QIcon("icons/cross.png")
+        deleteBtn = QtWidgets.QPushButton(deleteIcon,"Delete")
+        deleteBtn.setMaximumWidth(65)
+        deleteBtn.clicked.connect(self.delRecord)
+        fieldlayout.addRow(deleteBtn)
         self.update_user_fields()
         for key, value in self.ufields.items():
             fieldlayout.addRow(key, value)
-
-        updateBtn = QtWidgets.QPushButton("Update!")
-        updateBtn.clicked.connect(self.updateRecord)
+        UpdateIcon = QtGui.QIcon("icons/block--arrow.png")
+        updateBtn = QtWidgets.QPushButton(UpdateIcon, "Update")
+        updateBtn.clicked.connect(self.updateFormFields)
         fieldlayout.addRow(updateBtn)
 
         layout = QtWidgets.QGridLayout()
@@ -137,31 +148,33 @@ class MainWindow(QtWidgets.QMainWindow):
         layout.addWidget(self.labelEnd, 1, 2, Qt.AlignmentFlag.AlignLeft)
         layout.addWidget(self.displayEnd, 2, 2, Qt.AlignmentFlag.AlignLeft)
         layout.addWidget(self.btnGenerate, 3, 0)
+        layout.addWidget(self.openfile, 3, 4)
         layout.addWidget(self.table, 4, 4)
         layout.addWidget(fields_section, 4, 0)
         widget.setLayout(layout)
 
     def update_user_fields(self):
-        for val in self.user_fields.keys():
-            if self.user_fields[val]["controlType"] == "lineEdit":
+        for val in self.fieldsData.keys():
+            if self.fieldsData[val]["controlType"] == "lineEdit":
                 self.ufields[val] = QtWidgets.QLineEdit()
-            if self.user_fields[val]['controlType'] == 'checkbox':
+            if self.fieldsData[val]['controlType'] == 'checkbox':
                 self.ufields[val] = QtWidgets.QCheckBox()
 
-    def getFile(self):
+    def loadSaveData(self):
+        ''' 1. Loads file into saveData dictionary
+            2. Loads Fields section into user_fields dictionary
+        '''
         file = QtWidgets.QFileDialog.getOpenFileName(self, "Choose file")
         if file[0]:
             with open(file[0], "r") as F1:
                 self.saveData = json.load(F1)
-                self.user_fields = self.saveData['fields']
+                self.fieldsData = self.saveData['fields']
+            self.openfile.setText(file[0])
         else:
             print("Failed to load File")
-            self.statusTip = "Failed to Load File!"
-    
+            self.statusTip = "Failed to Load File"
 
-
-    def updateRecord(self):
-        self.setStatusTip("Time to update SaveData!")
+    def updateFormFields(self):
         key = self.ufields.get('key').text()  # Get key from field list
         if key:
             if self.saveData.get(key):  # Check in it exists in save data
@@ -170,20 +183,24 @@ class MainWindow(QtWidgets.QMainWindow):
             for ref, val in self.ufields.items():  # terrible var names..
                 if ref == 'key':
                     ...
-                elif self.user_fields[ref]['controlType'] == 'lineEdit':
+                elif self.fieldsData[ref]['controlType'] == 'lineEdit':
                     self.saveData[key][ref] = val.text()
-                elif self.user_fields[ref]['controlType'] == 'checkbox':
+                elif self.fieldsData[ref]['controlType'] == 'checkbox':
                     if val.checkState() == Qt.CheckState.Checked:
                         self.saveData[key][ref] = True
+
+    def delRecord(self):
+        key = self.ufields.get('key').text()
+        if self.saveData.get(key):
+                self.saveData.pop(key)
+        self.clearUfileds()
+
 
     def save(self):
         fileToSave = QtWidgets.QFileDialog.getSaveFileName(self, "WHERE?")
         if fileToSave[0]:
             with open(fileToSave[0], "w") as F1:
                 json.dump(self.saveData, F1, indent=4)
-        else:
-            print("Save Failed, did not get a file from file dialog")
-            # Todo: Make this message appear in status Bar.
 
     def print(self):
         print("doing the printing thing!")
@@ -191,9 +208,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
     def clearUfileds(self):
-        for key, control in self.ufields.items():
+        for key in self.ufields.keys():
             if type(self.ufields.get(key)) == QtWidgets.QLineEdit:
                 self.ufields[key].setText('')
+
+            elif type(self.fieldsData.get(key)) == QtWidgets.QCheckBox:
+                    self.ufields[key] == Qt.CheckState.Unchecked
+
 
     def showSelection(self):
         self.clearUfileds()
@@ -205,12 +226,12 @@ class MainWindow(QtWidgets.QMainWindow):
         if data:
             for name in data:
                 if type(self.ufields.get(name)) == QtWidgets.QLineEdit:
-                    text = self.saveData[selected_cidr].get(name)
+                    text = data.get(name)
                     self.ufields[name].setText(text)
                 else:
                     self.ufields[name].setText('')
                 if type(self.ufields.get(name)) == QtWidgets.QCheckBox:
-                    val = self.saveData[selected_cidr].get(name)
+                    val = data.get(name)
                     if val == 'True':
                         self.ufields[name].setCheckState(Qt.CheckState.Checked)
                     else:
@@ -218,22 +239,28 @@ class MainWindow(QtWidgets.QMainWindow):
                             Qt.CheckState.Unchecked)
 
     def merge(self, tableIn, records):
-        z = None
-        fillcolor = ''
+        cidrDetails = None
         for row in tableIn:
             for cell in row:
-                y = cell.get('network')
-                if y:
-                    z = records.get(y)
-                    if z:
-                        for key, value in z.items():
-                            show = self.user_fields[key]['show']
-                            if show == 'True':
-                                cell.update({key: value})
-                                if k:= self.user_fields.get(key):
-                                    fillcolor = k['colorMap'].get(value)
-                            if fillcolor:
-                                cell.update({'color': fillcolor})
+                fillcolor = ''
+                fillweight = 0
+                colorWeight = 0
+                cidr = cell.get('network')
+                if cidr:
+                    cidrDetails = records.get(cidr)  # SaveData records.
+                    if cidrDetails:
+                        for property, value in cidrDetails.items():
+                            if self.fieldsData[property]['show'] == 'True':
+                                cell[property] = value
+                                if self.saveData[cidr].get(property):
+                                    fillcolor = self.fieldsData[property]['colorMap'].get(value)
+                                    colorWeight = self.fieldsData[property].get("colorWeight")
+                                    if colorWeight == None:
+                                        colorWeight = 0
+                                    if colorWeight > fillweight:
+                                        fillweight = colorWeight
+                                        cell['color'] = fillcolor
+                                        print(f'cell is: {cell}')
 
     def generate(self):
         start = int(self.displayStart.text())
